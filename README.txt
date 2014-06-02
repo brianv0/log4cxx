783ca00b26a684004b818bf7e3b296fdf0ad0b41
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-06-02 17:06:54 +0200

    log4cxx unit tests doesn't works very well, disabling it.

---
+++ README.txt
@@ -0,0 +1,6 @@
+Required dependencies
+---------------------
+
+  | # On RH-like systems :
+  | yum install zip gzip 
+
--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,8 +2,12 @@
 
 CONFIGURE_OPTIONS="--prefix=${PREFIX} --with-apr=${APR_DIR} --with-apr-util=${APR_UTIL_DIR}"
 
-build()
-{
-    default_build
-    make check	
-}
+#build()
+#{
+#    default_build
+#    TODO : enable unit tests (i.e. make check) after build procedure,
+#	nevertheless log4cxx unit tests doesn't works very well, cf. :
+#	- https://issues.apache.org/jira/browse/LOGCXX-260
+#	- optionconvertertestcase also fails intermittently
+#    make check	
+#}
